a43a8286d9eb426dd0745032621d1c27cba8a742
Eduardo Sanzovo Quaggio <eduquaggio@gmail.com>
2026-05-31 17:06:50 -0300

    Add files via upload

---
+++ Semana 1/LeetCode 217.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        nums = np.array(nums)
+        tamanho_original = nums.size
+
+        nums = np.unique(nums)
+        tamanho_novo = nums.size
+
+        if (tamanho_novo == tamanho_original):
+            return False
+        else:
+            return True
